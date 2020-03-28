de2aa12a1bed9adbe744de2b73d3175ddef15649
Anil Gurung <anil_gurung15@yahoo.com>
2020-03-28 21:01:23 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -20,7 +20,7 @@ def words_five_letters_long(text)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-  binding.pry
+  # binding.pry
   if text.scan(/\b[A-Z]\w+[.,"':;!?\/-]/)
     true
   # elsif text.scan(/\b^[A-Z]\w+[.,"':;!?\/-]/) || text.scan(/\b[A-Z]\w+[.,"':;!?\/-]/) || text.scan(/\b^[A-Z]\w+[.,"':;!?\/-]/)
