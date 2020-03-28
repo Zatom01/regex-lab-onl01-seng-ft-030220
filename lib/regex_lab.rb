493c646dbc58dc3364baf9e215e9d8af764ead5d
Anil Gurung <anil_gurung15@yahoo.com>
2020-03-28 21:01:43 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -21,7 +21,7 @@ end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
   # binding.pry
-  if text.scan(/\b[A-Z]\w+[.,"':;!?\/-]/)
+  if text.match(/\b[A-Z]\w+[.,"':;!?\/-]/)
     true
   # elsif text.scan(/\b^[A-Z]\w+[.,"':;!?\/-]/) || text.scan(/\b[A-Z]\w+[.,"':;!?\/-]/) || text.scan(/\b^[A-Z]\w+[.,"':;!?\/-]/)
   #   false
