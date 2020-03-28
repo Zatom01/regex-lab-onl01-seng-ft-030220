679a6eb6cd1167cfb22e4b9975901f13998ec196
Anil Gurung <anil_gurung15@yahoo.com>
2020-03-28 19:54:59 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,17 +1,32 @@
 def starts_with_a_vowel?(word)
+  #It matches the beginning of the string \A followed by a vowel [aeiou] in a case-insensitive manner i returning a bool word.match?
+  if word.match(/\A[aeiou]/i)
+    true 
+  else
+    false 
+  end 
+end 
 
-end
 
 def words_starting_with_un_and_ending_with_ing(text)
+  text.scan(/un\w+ing/)
 
 end
 
 def words_five_letters_long(text)
+  text.scan(/\b[a-zA-Z]{5}\b/)#/b Matches a word-break.
 
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
+  if text.scan(/\b[A-Z]\w+[?]/)
+    true
+  else 
+    false 
+    
+  end 
+  
+  
 end
 
 def valid_phone_number?(phone)
